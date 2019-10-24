a07fa5990cf3beae39568da5aa7e96cc521e2e22
c256k145 <noremackientz@gmail.com>
2019-10-23 19:38:22 -0500

    idfk but it works

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,9 @@
 #include "Arduino.h"
 
 int duty_cycle_pin = 5;
-int input_pin = A7;
+int input_pin = A5;
 char serial_data = 0;
 float duty_cycle = 0.0;
-float arr[100]; 
 int arr_index = 0;
 
 float output = 12;
@@ -13,37 +12,23 @@ float min_voltage = 12.0;
 float arduino_5V = 4.88;
 float actual = 0;
 
-float R_small = 1000000;
-float R_big = 3000000;
+float R_small = 1010000;
+float R_big = 2693000;
 
 void writeDutyCycle(float value) {
+  value = (1-value);
   int write_val = round(value*255);
   analogWrite(duty_cycle_pin, write_val);
 }
 
-void firstSetup() {
-  for(int i =0; i < 100; i ++) {
-    arr[i] = analogRead(input_pin);
-  }
-}
-
-
 float measureVoltage() {
-  arr[arr_index] = analogRead(input_pin);
-  if(arr_index == 99) {
-    arr_index = 0;
+  float v_load = 0;
+  for(int i = 0;i < 10; i++) {
+    int sensor = analogRead(input_pin);
+    float measured_voltage = sensor*(arduino_5V/1024.0);
+    v_load += ((R_big+R_small)/R_small)*measured_voltage;
   }
-  else {
-    arr_index += 1;
-  }
-  float sum = 0;
-  for(int i = 0; i < 100; i++) {
-    sum += arr[i];
-  }
-  float avg = sum/100.0;
-  float measured_voltage = avg*(arduino_5V/1024.0);
-  float v_load = ((R_big+R_small)/R_small)*measured_voltage;
-  return(v_load);
+  return(v_load/10);
 }
 
 float adjustDutyCycle(float v_load) {
@@ -98,12 +83,15 @@ void setup() {
   pinMode(duty_cycle_pin, OUTPUT);
   pinMode(input_pin, INPUT);
   Serial.begin(9600);
-  setPwmFrequency(5,1); // Sets pin 5 to 62.5 KHz
-  firstSetup();
+  setPwmFrequency(duty_cycle_pin, 1); // Sets pwm output to 62.5 KHz
+//  firstSetup();
 }
 
 void loop() {
   delay(1);
+  actual = measureVoltage();
+  adjustDutyCycle(actual);
+  writeDutyCycle(duty_cycle);
   if(Serial.available() > 0) {
     serial_data = Serial.read();
 
@@ -119,7 +107,6 @@ void loop() {
         }
         break;
     }
-    
     Serial.print("Actual: ");
     Serial.println(measureVoltage());
     Serial.print("Desired Voltage: ");
@@ -128,7 +115,4 @@ void loop() {
     Serial.println(duty_cycle);
     Serial.println();
   }
-  actual = measureVoltage();
-  adjustDutyCycle(actual);
-  writeDutyCycle(duty_cycle);
 }
